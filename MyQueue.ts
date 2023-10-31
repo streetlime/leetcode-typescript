dabbc8ea204d8a33a5c895ad65c8705754bd64f4
chengjin <1004747472@qq.com>
2023-10-31 19:14:15 +0800

    ✨ feat: 栈实现队列

---
+++ MyQueue.ts
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=typescript
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+  private stackIn: number[];
+  private stackOut: number[];
+  constructor() {
+    this.stackIn = [];
+    this.stackOut = [];
+  }
+
+  push(x: number): void {
+    this.stackIn.push(x);
+  }
+
+  pop(): number {
+    if (this.stackOut.length === 0) {
+      while (this.stackIn.length > 0) {
+        this.stackOut.push(this.stackIn.pop()!);
+      }
+    }
+    return this.stackOut.pop()!;
+  }
+
+  peek(): number {
+    const res: number = this.pop();
+    this.stackOut.push(res);
+    return res;
+  }
+
+  empty(): boolean {
+    return this.stackIn.length === 0 && this.stackOut.length === 0;
+  }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
